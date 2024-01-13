f5b1ae80fba51277d8e76d15e954a8a493dad0e3
Lakshita Mukati <102685551+LAKSHITAAAAA@users.noreply.github.com>
2024-01-13 16:38:39 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print("Hello World")
+#print is a function in python
+#function is any block of code written for our convinence which is reusable
+#print statement takes objects and display it as an output by converting them to string first.
+#syntax print(object(s), sep=separator, end=end, file=file, flush=flush)
+#object:-variables that contain data and functions that can be used to manipulate the data.
+#sep :- separator between object or string. It defines the character to be printed between values.
+#end  : defines the character to be printed at the end of each object or string.
+#file :- specify the file object where the information has to be printed. By default it prints on console screen.
+#flush :-It cleans the internal buffer (a temporary storage space for storing the data which is intended to be written to main memory)
+#It is a binary value True for flushing and False for not.By default it is False
