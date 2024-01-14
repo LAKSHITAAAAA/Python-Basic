2985d9539f19b2a1c59055c0ea1842700ea21111
Lakshita Mukati <102685551+LAKSHITAAAAA@users.noreply.github.com>
2024-01-14 18:06:49 +0530

    Add files via upload

---
+++ Comments&Escape Sequence Characters.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+# it is used for commenting in python
+print("Hey There! \nHow are you?")#newline character
+print("Hey There! \tHow are you?")# tab
+print("Hey There! \b How are you?")#backspace
+print("Hey There! \r How are you?")#carriage return
+print("Hey There! \f How are you?")#form feed
+print("Hey There! \"How are you?")# double quote
+print("Hey There! \\How are you?")#backslash
+
+
+#\n is a new line character it jumps to the next line, it is also called as an escape sequence character
+#Escape Sequence character(ESC):-characters which are not just characters but has some other commands or meanings
+#types of ESCs:1. \n:new line character 2.\t:-tab 3.\b:-backspace 4.\r:-carriage return(it just prints the string given after starting from the starting point and removes whatever is written before it) 5.\f:-form feed (it tells when to break the page while printing) 6.\':-single qoute
+#7.\":-double qoute 8.\\:-inserts backslash
+ctrl+/ to comment multiple lines after selecting the lines which we want to comment.
+'''(triple quotes and triple double quotes) are used for multi-line commenting Python is an interpretd language that means it runs the code line by line and exceutes it as well where as compiler checks the whole code at once and makes it's object file which gets converted to .exe file and then gets executed.
