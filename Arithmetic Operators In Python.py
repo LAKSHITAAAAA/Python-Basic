f96c2829815ad6184b4efc4ef98b915e772f56c8
Lakshita Mukati <102685551+LAKSHITAAAAA@users.noreply.github.com>
2024-01-15 23:07:09 +0530

    Add files via upload

---
+++ Arithmetic Operators In Python.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+Arithmetic Operators In Python:-
+1.Addition(+):-Add 2 numbers
+2.Subtraction(-):-Subtract 2 numbers
+3.Multiplication(*):-Multiply 2 numbers
+4.Division(/):-Divides 2 numbers
+5.Floor Division(//):-just returns the whole number value after performing the division
+6.modolus(%):-returns the remainder after performing the division
+7.Exponent(**)
+
+# In[2]:
+
+
+print(5+2)
+print(5*2)
+print(5-2)
+print(5/2)
+print(5//2)
+print(5%2)
+print(5**2)
+
+
+# In[3]:
+
+
+#Calculator
+a=int(input("Enter the number1: "))
+b=int(input("Enter the number2: "))
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Division: ",a/b)
+print("Multiplication: ",a*b)
+
+
+
+# In[ ]:
+
+
+
+
