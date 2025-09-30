a76cacdb979fde8220860f8ff59af99abbc347d9
Lakshita Mukati <102685551+LAKSHITAAAAA@users.noreply.github.com>
2025-09-30 22:52:02 +0530

    Add files via upload

---
+++ short hand if else.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Short Hand if- else:
+
+# In[1]:
+
+
+a=20
+b=50
+print("hi") if a>b else print("bye") #everything in a single line
+
+
+# # Syntax
+result=value_if_else if condition else value_if_else
+which is equivalent to ;
+if condition:
+    statement(s)
+else:
+    statement(s)
+#we can use short hand whenever it is possible to do so but not in every case. 
